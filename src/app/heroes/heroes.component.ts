6a7fa93515ad7d9ebbd331380b5b1088132a5704
sithtraly <sithtraly147@gmail.com>
2020-10-05 15:42:12 +0700

    Add mock hero and use some event

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import {Heroes} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,6 +12,17 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Lee Sin'
   };
+
+  heroes = Heroes;
+
+  selectedHero: Hero;
+  onClick(hero: Hero): void{ // onClick is a function name
+    this.selectedHero = hero;
+  }
+
+  clear():void{
+    this.selectedHero = null;
+  }
 
   constructor() { }
 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { from } from 'rxjs';
+import {Hero} from './hero';
+
+export const Heroes: Hero[] = [
+    {id: 1, name: 'Ling'},
+    {id: 2, name: 'Alucard'},
+    {id: 3, name: 'Miya'},
+    {id: 4, name: 'Wanwan'}
+];
